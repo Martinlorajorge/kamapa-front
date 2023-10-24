4aaa31c3ff1fda2e99fe92d936e79fa9f957960c
Martin Aaron Lora Jorge <66756571+Martinlorajorge@users.noreply.github.com>
2023-10-24 13:49:13 -0300

    middleware configuration on

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,21 @@
-export default function HomePage () {
+"use client"
+import { useSession } from "next-auth/react";
+
+const HomePage = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  console.log(session);
+
   return (
-    <h1>Home o Inicio</h1>
-  )
-}
+    <div>
+      <h1>Dashboard</h1>
+      <pre>
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
+    </div>
+  );
+};
+export default Dashboard;
