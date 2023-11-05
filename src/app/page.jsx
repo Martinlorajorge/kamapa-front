066dbea11fe6f3aba0a531db78c12df38da7ba53
Martin Aaron Lora Jorge <martinlorajorge@gmail.com>
2023-11-04 22:59:18 -0300

    arreglo del boton
    position

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,57 +1,53 @@
 'use client'
-import { Container } from 'react-bootstrap';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
-import Image from 'next/image';
-import Link from 'next/link';
-function DarkVariantExample() {
+import { Container } from 'react-bootstrap'
+import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button'
+import Image from 'next/image'
+import Link from 'next/link'
+function DarkVariantExample () {
   return (
-    <div style={{ width:'100vw', height:'100vh', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'space-evenly', zIndex:'-1'}}>
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
       <Image
-        src="/backLogin.jpg"
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
+        src='/backLogin.jpg'
+        alt='Background'
+        layout='fill'
+        objectFit='cover'
       />
-      <Carousel data-bs-theme="dark" style={{backgroundColor:'transparent', width:'50vw', height:'50vh'}}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/bienvenida2.png"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/bienvenida2.png"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/bienvenida2.png"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-    <style type='text/css'>
-            {`
-                .btn-flat {
-                  background-color: purple;
-                  color: white;
-                }
+      <Carousel data-bs-theme='dark' style={{ backgroundColor: 'transparent', width: '50vw', height: '50vh' }}>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/bienvenida2.png'
+            alt='First slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/bienvenida2.png'
+            alt='Second slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='/bienvenida2.png'
+            alt='Third slide'
+          />
+        </Carousel.Item>
+      </Carousel>
 
-                .btn-xxl {
-                  padding: 0.4rem 1rem;
-                  font-size: 1rem;
-                }
-              `}
-          </style>
-    <Link variant='flat' size='xxl' className='btn-flat' href='/Login' type='text'> Comencemos </Link>
+      <Link href='/login' style={{ borderRadius: '10px', backgroundColor: 'purple', color: 'white', padding: '0.2rem 0.8rem', fontSize: '1rem', zIndex: '100' }}>
+        <Button
+          type='submit'
+          style={{ backgroundColor: 'purple', borderColor: 'purple' }}
+        >
+          Comenzar
+        </Button>
+      </Link>
+
     </div>
-  );
+  )
 }
 
-export default DarkVariantExample;
+export default DarkVariantExample
