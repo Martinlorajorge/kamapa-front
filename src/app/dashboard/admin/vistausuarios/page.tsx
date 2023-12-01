90aeeeb24cd63b124240894da78f8e7ee6e79516
Martin Aaron Lora Jorge <martinlorajorge@gmail.com>
2023-12-01 00:07:13 -0300

    retoques middleware y edit, consulta y eliminado de empleado

--- src/app/dashboard/admin/vistausuarios/page.tsx
+++ src/app/dashboard/admin/vistausuarios/page.tsx
@@ -1,261 +1,342 @@
-'use client'
+'use client';
 import { useState, useEffect } from 'react';
 import { Button, Table, Modal, Form } from 'react-bootstrap';
 import { BsEye, BsPencil, BsTrash } from 'react-icons/bs';
 import Link from 'next/link';
 
 const VistaEmpleadosPage = () => {
-  const [empleados, setEmpleados] = useState([]);
-  const [selectedEmpleado, setSelectedEmpleado] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [showConfirmModal, setShowConfirmModal] = useState(false);
+	const [empleados, setEmpleados] = useState([]);
+	const [selectedEmpleado, setSelectedEmpleado] = useState(null);
+	const [showModal, setShowModal] = useState(false);
+	const [showEditModal, setShowEditModal] = useState(false);
+	const [showConfirmModal, setShowConfirmModal] = useState(false);
+	const [showSaveConfirmModal, setShowSaveConfirmModal] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado`,
-        );
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado`,
+				);
 
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}: ${response.statusText}`);
-        }
+				if (!response.ok) {
+					throw new Error(`Error ${response.status}: ${response.statusText}`);
+				}
 
-        const data = await response.json();
-        setEmpleados(data.empleados);
-        console.log(data);
-      } catch (error) {
-        console.error('Error al obtener empleados:', error.message);
-      }
-    };
+				const data = await response.json();
+				setEmpleados(data.empleados);
+				console.log(data);
+			} catch (error) {
+				console.error('Error al obtener empleados:', error.message);
+			}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  const handleConsultar = (empleado) => {
-    setSelectedEmpleado(empleado);
-    setShowModal(true);
-  };
+	const handleConsultar = (empleado) => {
+		setSelectedEmpleado(empleado);
+		setShowModal(true);
+	};
 
-  const handleModificar = (empleado) => {
-    setSelectedEmpleado(empleado);
-    setShowEditModal(true);
-  };
+	const handleEliminar = (empleado) => {
+		setSelectedEmpleado(empleado);
+		setShowConfirmModal(true); // Muestra el modal de confirmación
+	};
 
+	const handleConfirmDelete = async () => {
+		setShowConfirmModal(false); // Cierra el modal de confirmación
 
-  const handleConfirmDelete = async () => {
-    setShowConfirmModal(false); // Cierra el modal de confirmación
-  
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado/${selectedEmpleado.id}`,
-        {
-          method: 'DELETE',
-        }
-      );
-  
-      if (!response.ok) {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
-  
-      setEmpleados(empleados.filter((empleado) => empleado.id !== selectedEmpleado.id));
-      console.log(`Empleado con ID ${selectedEmpleado.id} eliminado`);
-    } catch (error) {
-      console.error('Error al eliminar empleado:', error.message);
-    }
-  };
+		try {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado/${selectedEmpleado.id}`,
+				{
+					method: 'DELETE',
+				},
+			);
 
-  const handleEliminar = (empleado) => {
-  setSelectedEmpleado(empleado);
-  setShowConfirmModal(true); // Muestra el modal de confirmación
-};
+			if (!response.ok) {
+				throw new Error(`Error ${response.status}: ${response.statusText}`);
+			}
 
-  const handleSave = async () => {
-    try {
-      const legajo = document.getElementById('formLegajo').value;
-      const updatedEmpleado = {
-        ...selectedEmpleado,
-        UsuarioEmpleado: {
-          ...selectedEmpleado.UsuarioEmpleado,
-          legajo: legajo,
-        },
-      };
+			setEmpleados(
+				empleados.filter((empleado) => empleado.id !== selectedEmpleado.id),
+			);
+			console.log(`Empleado con ID ${selectedEmpleado.id} eliminado`);
+		} catch (error) {
+			console.error('Error al eliminar empleado:', error.message);
+		}
+	};
 
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado/${selectedEmpleado.id}`,
-        {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updatedEmpleado),
-        }
-      );
+	const handleModificar = (empleado) => {
+		setSelectedEmpleado(empleado);
+		setShowEditModal(true);
+	};
 
-      if (!response.ok) {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
+	const handleSave = () => {
+		// Muestra el modal de confirmación de guardado en lugar de guardar directamente
+		setShowSaveConfirmModal(true);
+	};
 
-      setEmpleados(
-        empleados.map((empleado) =>
-          empleado.id === selectedEmpleado.id ? updatedEmpleado : empleado
-        )
-      );
-      console.log(`Empleado con ID ${selectedEmpleado.id} actualizado`);
-      setShowEditModal(false);
-    } catch (error) {
-      console.error('Error al actualizar empleado:', error.message);
-    }
-  };
+	const handleConfirmSave = async () => {
+		try {
+			const legajo = (document.getElementById('formLegajo') as HTMLInputElement)
+				.value;
+			// Agrega más campos según sea necesario
 
-  return (
-    <div className='p-3'>
-      <Link href='/dashboard/admin/vistausuarios/regempleado'>
-        <Button
-          variant='flat'
-          style={{
-            backgroundColor: 'purple',
-            color: 'white',
-            padding: '0.4rem 1rem',
-            fontSize: '1rem',
-            marginBottom: '1rem',
-            transition: 'all 0.3s ease',
-          }}
-          onMouseEnter={(e) => {
-            e.currentTarget.style.backgroundColor = 'white';
-            e.currentTarget.style.color = 'black';
-          }}
-          onMouseLeave={(e) => {
-            e.currentTarget.style.backgroundColor = 'purple';
-            e.currentTarget.style.color = 'white';
-          }}>
-          Registrar Empleado
-        </Button>
-      </Link>
+			const updatedEmpleado = {
+				...selectedEmpleado,
+				UsuarioEmpleado: {
+					...selectedEmpleado.UsuarioEmpleado,
+					legajo: legajo,
+					// Agrega más campos según sea necesario
+				},
+			};
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Legajo</th>
-            <th>Nombre</th>
-            <th>Telefono</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(empleados) && empleados.length > 0 ? (
-            empleados.map((empleado) => (
-              <tr key={empleado.id}>
-                <td>{empleado.UsuarioEmpleado.legajo}</td>
-                <td>
-                  {empleado.UsuarioEmpleado &&
-                    `${empleado.UsuarioEmpleado.nombre}, ${empleado.UsuarioEmpleado.apellido}`}
-                </td>
-                <td>
-                  {empleado.UsuarioEmpleado &&
-                    empleado.UsuarioEmpleado.telefono}
-                </td>
-                <td>
-                  <Button
-                    variant='link'
-                    onClick={() => handleConsultar(empleado)}
-                    title='Consultar Empleado'>
-                    <BsEye />
-                  </Button>
-                  <Button
-                    variant='link'
-                    onClick={() => handleModificar(empleado)}
-                    title='Modificar Empleado'>
-                    <BsPencil />
-                  </Button>
-                  <Button
-                    variant='link'
-                    onClick={() => handleEliminar(empleado.id)}
-                    title='Eliminar Empleado'>
-                    <BsTrash />
-                  </Button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan='4'>No hay empleados disponibles</td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/empleado/${selectedEmpleado.id}`,
+				{
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(updatedEmpleado),
+				},
+			);
+			console.log(updatedEmpleado);
+			if (!response.ok) {
+				throw new Error(`Error ${response.status}: ${response.statusText}`);
+			}
 
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Detalles del Empleado</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {selectedEmpleado && (
-            <>
-              <p>Legajo: {selectedEmpleado.UsuarioEmpleado.legajo}</p>
-              <p>Fecha de ingreso: {new Date(selectedEmpleado.UsuarioEmpleado.fecha_ingreso).toLocaleDateString()}</p>
-              <p>Fecha de egreso: {selectedEmpleado.UsuarioEmpleado.fecha_egreso ? new Date(selectedEmpleado.UsuarioEmpleado.fecha_egreso).toLocaleDateString() : 'N/A'}</p>
-              <p>Nombre: {selectedEmpleado.UsuarioEmpleado.nombre}</p>
-              <p>Apellido: {selectedEmpleado.UsuarioEmpleado.apellido}</p>
-              <p>DNI: {selectedEmpleado.UsuarioEmpleado.dni}</p>
-              <p>CUIL: {selectedEmpleado.UsuarioEmpleado.cuil}</p>
-              <p>Fecha de nacimiento: {new Date(selectedEmpleado.UsuarioEmpleado.fechaNacimiento).toLocaleDateString()}</p>
-              <p>Teléfono: {selectedEmpleado.UsuarioEmpleado.telefono}</p>
-              <p>Estado: {selectedEmpleado.UsuarioEmpleado.is_active ? 'Activo' : 'Inactivo'}</p>
-            </>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Cerrar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+			const data = await response.json();
+			console.log('Response:', data);
 
-      <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Editar Empleado</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {selectedEmpleado && (
-            <Form>
-              <Form.Group controlId="formLegajo">
-                <Form.Label>Legajo</Form.Label>
-                <Form.Control type="text" defaultValue={selectedEmpleado.UsuarioEmpleado.legajo} />
-              </Form.Group>
-              {/* Agrega más campos de formulario según sea necesario */}
-            </Form>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowEditModal(false)}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleSave}>
-            Guardar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+			setEmpleados((prevEmpleados) =>
+				prevEmpleados.map((empleado) =>
+					empleado.id === selectedEmpleado.id ? data : empleado,
+				),
+			);
+			console.log(`Empleado con ID ${selectedEmpleado.id} actualizado`);
+			setShowEditModal(false);
+			setShowSaveConfirmModal(false); // Cierra el modal de confirmación de guardado
+		} catch (error) {
+			console.error('Error al actualizar empleado:', error.message);
+		}
+	};
+	return (
+		<div className='p-3'>
+			<Link href='/dashboard/admin/vistausuarios/regempleado'>
+				<Button
+					variant='flat'
+					style={{
+						backgroundColor: 'purple',
+						color: 'white',
+						padding: '0.4rem 1rem',
+						fontSize: '1rem',
+						marginBottom: '1rem',
+						transition: 'all 0.3s ease',
+					}}
+					onMouseEnter={(e) => {
+						e.currentTarget.style.backgroundColor = 'white';
+						e.currentTarget.style.color = 'black';
+					}}
+					onMouseLeave={(e) => {
+						e.currentTarget.style.backgroundColor = 'purple';
+						e.currentTarget.style.color = 'white';
+					}}>
+					Registrar Empleado
+				</Button>
+			</Link>
 
-      <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmar eliminación</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          ¿Estás seguro de que quieres eliminar a este empleado?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowConfirmModal(false)}>
-            No
-          </Button>
-          <Button variant="danger" onClick={handleConfirmDelete}>
-            Sí, eliminar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+			<Table
+				striped
+				bordered
+				hover>
+				<thead>
+					<tr>
+						<th>Legajo</th>
+						<th>Nombre</th>
+						<th>Telefono</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					{Array.isArray(empleados) && empleados.length > 0 ? (
+						empleados.map((empleado) => (
+							<tr key={empleado.id}>
+								<td>{empleado?.UsuarioEmpleado?.legajo}</td>
+								<td>
+									{empleado.UsuarioEmpleado &&
+										`${empleado.UsuarioEmpleado.nombre}, ${empleado.UsuarioEmpleado.apellido}`}
+								</td>
+								<td>
+									{empleado.UsuarioEmpleado &&
+										empleado.UsuarioEmpleado.telefono}
+								</td>
+								<td>
+									<Button
+										variant='link'
+										onClick={() => handleConsultar(empleado)}
+										title='Consultar Empleado'>
+										<BsEye />
+									</Button>
+									<Button
+										variant='link'
+										onClick={() => handleModificar(empleado)}
+										title='Modificar Empleado'>
+										<BsPencil />
+									</Button>
+									<Button
+										variant='link'
+										onClick={() => handleEliminar(empleado.id)}
+										title='Eliminar Empleado'>
+										<BsTrash />
+									</Button>
+								</td>
+							</tr>
+						))
+					) : (
+						<tr>
+							<td colSpan='4'>No hay empleados disponibles</td>
+						</tr>
+					)}
+				</tbody>
+			</Table>
+
+			<Modal
+				show={showModal}
+				onHide={() => setShowModal(false)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Detalles del Empleado</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{selectedEmpleado && (
+						<>
+							<p>Legajo: {selectedEmpleado?.UsuarioEmpleado?.legajo}</p>
+							<p>
+								Fecha de ingreso:{' '}
+								{new Date(
+									selectedEmpleado?.UsuarioEmpleado?.fecha_ingreso,
+								).toLocaleDateString()}
+							</p>
+							<p>
+								Fecha de egreso:{' '}
+								{selectedEmpleado?.UsuarioEmpleado?.fecha_egreso
+									? new Date(
+											selectedEmpleado?.UsuarioEmpleado?.fecha_egreso,
+									  ).toLocaleDateString()
+									: 'N/A'}
+							</p>
+							<p>Nombre: {selectedEmpleado?.UsuarioEmpleado?.nombre}</p>
+							<p>Apellido: {selectedEmpleado?.UsuarioEmpleado?.apellido}</p>
+							<p>DNI: {selectedEmpleado?.UsuarioEmpleado?.dni}</p>
+							<p>CUIL: {selectedEmpleado?.UsuarioEmpleado?.cuil}</p>
+							<p>
+								Fecha de nacimiento:{' '}
+								{new Date(
+									selectedEmpleado?.UsuarioEmpleado?.fechaNacimiento,
+								).toLocaleDateString()}
+							</p>
+							<p>Teléfono: {selectedEmpleado?.UsuarioEmpleado?.telefono}</p>
+							<p>
+								Estado:{' '}
+								{selectedEmpleado?.UsuarioEmpleado?.is_active
+									? 'Activo'
+									: 'Inactivo'}
+							</p>
+						</>
+					)}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button
+						variant='secondary'
+						onClick={() => setShowModal(false)}>
+						Cerrar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+
+			<Modal
+				show={showEditModal}
+				onHide={() => setShowEditModal(false)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Editar Empleado</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{selectedEmpleado && (
+						<Form>
+							<Form.Group controlId='formLegajo'>
+								<Form.Label>Legajo</Form.Label>
+								<Form.Control
+									type='text'
+									defaultValue={selectedEmpleado?.UsuarioEmpleado?.legajo}
+								/>
+							</Form.Group>
+							{/* Agrega más campos de formulario según sea necesario */}
+						</Form>
+					)}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button
+						variant='secondary'
+						onClick={() => setShowEditModal(false)}>
+						Cancelar
+					</Button>
+					<Button
+						variant='primary'
+						onClick={handleSave}>
+						Guardar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+
+			<Modal
+				show={showConfirmModal}
+				onHide={() => setShowConfirmModal(false)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Confirmar eliminación</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					¿Estás seguro de que quieres eliminar a este empleado?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button
+						variant='secondary'
+						onClick={() => setShowConfirmModal(false)}>
+						No
+					</Button>
+					<Button
+						variant='danger'
+						onClick={handleConfirmDelete}>
+						Sí, eliminar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+
+			<Modal
+				show={showSaveConfirmModal}
+				onHide={() => setShowSaveConfirmModal(false)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Confirmar cambios</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					¿Estás seguro de que quieres guardar los cambios?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button
+						variant='secondary'
+						onClick={() => setShowSaveConfirmModal(false)}>
+						Cancelar
+					</Button>
+					<Button
+						variant='primary'
+						onClick={handleConfirmSave}>
+						Confirmar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 };
 
 export default VistaEmpleadosPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,42 @@
-export { default } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*']
-}
+	matcher: ['/dashboard/:path*'],
+};
+// import { NextApiResponse, NextApiRequest } from 'next';
+// import { getSession } from 'next-auth/react';
+// import { NextResponse } from 'next/server';
+
+// // Define los roles y sus permisos
+// const roles = {
+// 	admin: ['/dashboard/*'], // El admin puede acceder a todas las rutas de /dashboard
+// 	director: ['/dashboard/reports', '/dashboard/overview'], // El director puede acceder a /dashboard/reports y /dashboard/overview
+// 	secretario: ['/dashboard/attendance'], // El secretario puede acceder a /dashboard/attendance
+// 	preceptor: ['/dashboard/students'], // El preceptor puede acceder a /dashboard/students
+// 	docente: ['/dashboard/classes'], // El docente puede acceder a /dashboard/classes
+// 	alumno: ['/dashboard/profile'], // El alumno puede acceder a /dashboard/profile
+// 	// Agrega más roles y permisos según sea necesario
+// };
+
+// export default async function handler(
+// 	req: NextApiRequest,
+// 	res: NextApiResponse,
+// 	next,
+// ) {
+// 	const session = await getSession({ req });
+
+// 	if (session) {
+// 		const userRole = session.user.rol;
+// 		const userPermissions = roles[userRole];
+
+// 		if (userPermissions && userPermissions.includes(req.url)) {
+// 			// Si el usuario tiene permiso para acceder a la ruta, pasa al siguiente middleware
+// 			next();
+// 		} else {
+// 			// Si el usuario no tiene permiso para acceder a la ruta, devuelve un error
+// 			return NextResponse.error(403, 'Acceso denegado');
+// 		}
+// 	} else {
+// 		return NextResponse.error(401, 'No autenticado');
+// 	}
+// }
